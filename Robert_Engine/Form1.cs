b42b24faf18d8ef2c75ce39340654ca31a80d6be
notsu <literallysum@gmail.com>
2022-08-05 10:08:11 +0500

    Update Form1.cs

--- Robert_Engine/Form1.cs
+++ Robert_Engine/Form1.cs
@@ -21,19 +21,14 @@ namespace Robert_Engine
         {
             var timer1 = new System.Windows.Forms.Timer();
             if (milliseconds == 0 || milliseconds < 0) return;
-
-            // Console.WriteLine("start wait timer");
             timer1.Interval = milliseconds;
             timer1.Enabled = true;
             timer1.Start();
-
             timer1.Tick += (s, e) =>
             {
                 timer1.Enabled = false;
                 timer1.Stop();
-                // Console.WriteLine("stop wait timer");
             };
-
             while (timer1.Enabled)
             {
                 Application.DoEvents();
